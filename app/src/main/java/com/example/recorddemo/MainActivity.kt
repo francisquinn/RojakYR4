5d71f9079d04cf3ee3fab474268cce5e610a3c78
francisquinn <francisquinnn@gmail.com>
2020-11-24 15:03:13 +0000

    Audio recorder prototype
    - Branch created for the audio recorder prototype
    - Record audio to WAV format
    - Store audio file in internal storage of phone

--- app/src/main/java/com/example/recorddemo/MainActivity.kt
+++ app/src/main/java/com/example/recorddemo/MainActivity.kt
@@ -10,18 +10,23 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.github.squti.androidwaverecorder.WaveRecorder
 import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
 
-    var recorder: MediaRecorder? = null
-    private var fileName: String = ""
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        fileName = "${externalCacheDir?.absolutePath}/audiorecordtest.wav"
+        val filePath:String = externalCacheDir?.absolutePath + "/audioFile.wav"
+
+        val waveRecorder = WaveRecorder(filePath)
+        waveRecorder.waveConfig.sampleRate = 44100
+        waveRecorder.noiseSuppressorActive = true
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,58 +37,25 @@ class MainActivity : AppCompatActivity() {
 
         startButton.setOnClickListener {
             if (ContextCompat.checkSelfPermission(this,
-                            Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
                 ActivityCompat.requestPermissions(this, permissions,0)
             } else {
-                startRecording()
+                waveRecorder.startRecording()
                 Toast.makeText(this, "Recording started!", Toast.LENGTH_SHORT).show()
             }
         }
 
         stopButton.setOnClickListener{
-            stopRecording()
+            waveRecorder.stopRecording()
             Toast.makeText(this, "Recording stopped!", Toast.LENGTH_SHORT).show()
         }
 
-       // button_pause_recording.setOnClickListener {
-           // pauseRecording()
-        //}
-    }
 
-    private fun startRecording() {
-        recorder = MediaRecorder().apply {
-            setAudioSource(MediaRecorder.AudioSource.MIC)
-            setAudioEncodingBitRate(128000)
-            setAudioSamplingRate(96000)
-            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-            setOutputFile(fileName)
-            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
-
-            try {
-                prepare()
-                Log.i("AudioRecordTest", "Recording started")
-            } catch (e: IOException) {
-                Log.e("AudioRecordTest", "prepare() failed")
-            }
-
-            start()
-        }
     }
 
 
 
-    private fun stopRecording() {
-        recorder?.apply {
-            stop()
-            release()
-
-        }
-
-        Log.i("AudioRecordTest", "Recording stopped!")
-        Log.i("AudioRecordTest", "${fileName}")
-        recorder = null
-    }
 
 }
