b8145f60d68715217743a0309ec7fa077d74850c
francisquinn <francisquinnn@gmail.com>
2021-01-29 12:42:54 +0000

    Stop/Start audio file upload to Google Cloud Storage
    - Stop audio every 10 seconds then sends to the Cloud Storage for testing purposes
    - Automatic uploading
    - Stops uploading when stop button is pressed
    - Words that overlap are not recognized

--- app/src/main/java/com/example/recorddemo/MainActivity.kt
+++ app/src/main/java/com/example/recorddemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.recorddemo
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,16 +11,20 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.recorddemo.R
 import com.github.squti.androidwaverecorder.WaveRecorder
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.ktx.storage
 import com.google.firebase.storage.ktx.storageMetadata
 import java.io.File
 import com.google.firebase.storage.ktx.component1
 import com.google.firebase.storage.ktx.component2
-
 import java.io.IOException
+import java.util.Timer
+import kotlin.concurrent.schedule
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,22 +38,17 @@ class MainActivity : AppCompatActivity() {
         // Points to the root reference
         val storageRef = storage.reference
 
-        // Name of the recorded file
-        val fileName = "audioFileFireThree.wav"
+        val timer = Timer()
+        var num = 0
 
-        // filePath located in the cache
-        val filePath: String = externalCacheDir?.absolutePath + "/audioFile.wav"
-
-        // Record the WAV file to the filePath cache
-        val waveRecorder = WaveRecorder(filePath)
+        var fileName = "/audioFile$num.wav"
+        var filePath: String = externalCacheDir?.absolutePath + fileName
+        var waveRecorder = WaveRecorder(filePath)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val startButton: Button = findViewById(R.id.button_start_recording)
         val stopButton: Button = findViewById(R.id.button_stop_recording)
-
-        // Start Button
         startButton.setOnClickListener {
             if (ContextCompat.checkSelfPermission(
                     this,
@@ -65,44 +65,73 @@ class MainActivity : AppCompatActivity() {
                 )
                 ActivityCompat.requestPermissions(this, permissions, 0)
             } else {
-                waveRecorder.startRecording()
+
+                timer.scheduleAtFixedRate(timerTask {
+                    //waveRecorder.startRecording()
+                    Log.d("Recorder", "Recording stopped")
+                    waveRecorder.stopRecording()
+                    println("StopRecording")
+
+                    cloudUploader(filePath, fileName, storageRef)
+
+                    num++
+                    fileName = "/audioFile$num.wav"
+                    filePath = externalCacheDir?.absolutePath + fileName
+                    waveRecorder = WaveRecorder(filePath)
+                }, 10000, 10000)
+
+                timer.scheduleAtFixedRate(timerTask {
+                    waveRecorder.startRecording()
+                    Log.d("Recorder", "Recording started")
+                }, 1, 10000)
+
                 Toast.makeText(this, "Recording started!", Toast.LENGTH_SHORT).show()
             }
         }
-
-        // Stop Button
         stopButton.setOnClickListener {
+            timer.cancel()
+            timer.purge()
             waveRecorder.stopRecording()
             Toast.makeText(this, "Recording stopped!", Toast.LENGTH_SHORT).show()
+            Log.d("Recorder", "Recording stopped")
 
-            // Retrieve the file from the filePath
-            val file = Uri.fromFile(File(filePath))
+            cloudUploader(filePath, fileName, storageRef)
+            
+            num++
+            fileName = "/audioFile$num.wav"
+            filePath = externalCacheDir?.absolutePath + fileName
+            waveRecorder = WaveRecorder(filePath)
+        }
+    }
 
-            // Type of metadata
-            val metadata = storageMetadata {
-                contentType = "audio/wav"
-            }
+    private fun cloudUploader(filePath:String, fileName:String, storageRef:StorageReference){
+        // Retrieve the file from the filePath
+        val file = Uri.fromFile(File(filePath))
 
-            // Upload to the Bucket
-            val uploadTask = storageRef.child(fileName).putFile(file, metadata)
-
-            // Listen for state changes, errors, and completion of the upload.
-            // You'll need to import com.google.firebase.storage.ktx.component1 and
-            // com.google.firebase.storage.ktx.component2
-            uploadTask.addOnProgressListener { (bytesTransferred, totalByteCount) ->
-                val progress = (100.0 * bytesTransferred) / totalByteCount
-                Log.d("Uploader", "Upload is $progress% done")
-            }.addOnPausedListener {
-                Log.d("Uploader", "Upload is paused")
-            }.addOnFailureListener {
-                // Handle unsuccessful uploads
-            }.addOnSuccessListener {
-                // Handle successful uploads on complete
-                // ...
-            }
-
-
+        // Type of metadata
+        val metadata = storageMetadata {
+            contentType = "audio/wav"
         }
 
+        Log.d("Recorder", "Uploading to the cloud...")
+
+        // Upload to the Bucket
+        val uploadTask = storageRef.child(fileName).putFile(file, metadata)
+
+        // Listen for state changes, errors, and completion of the upload.
+        // You'll need to import com.google.firebase.storage.ktx.component1 and
+        // com.google.firebase.storage.ktx.component2
+        uploadTask.addOnProgressListener { (bytesTransferred, totalByteCount) ->
+            val progress = (100.0 * bytesTransferred) / totalByteCount
+            Log.d("Recorder", "Upload is $progress% done")
+        }.addOnPausedListener {
+            Log.d("Recorder", "Upload is paused")
+        }.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener {
+
+        }
     }
+
+
 }
