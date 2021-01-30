f990f72215c6a4970c4e20099b051e9adb99b6ee
francisquinn <francisquinnn@gmail.com>
2021-01-30 16:21:07 +0000

    Updated mobile_app branch with Shikui branch
    - Merged login/register from shikui branch into the mobile_app branch
    - Updated color theme

--- app/src/main/java/com/example/tactalk/MainActivity.kt
+++ app/src/main/java/com/example/tactalk/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.tactalk
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.tactalk.activity.LoginFragment
+import com.example.tactalk.activity.RegisterFragment
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_app_start_page)
+
     }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_login_page -> {
+                // go to login page
+                startActivity(Intent(this, LoginFragment::class.java))
+            }
+            R.id.btn_register_page -> {
+                //go to register page
+                startActivity(Intent(this, RegisterFragment::class.java))
+            }
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/example/tactalk/activity/LoginFragment.kt
@@ -0,0 +1,72 @@
+package com.example.tactalk.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tactalk.R
+import com.example.tactalk.network.TacTalkAPI
+import com.example.tactalk.network.RetrofitClient
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : AppCompatActivity() {
+
+    lateinit var tacTalkAPI: TacTalkAPI
+    internal var compositeDisposable = CompositeDisposable()
+
+    override fun onStop() {
+        compositeDisposable.clear();
+        super.onStop()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_login)
+
+        //API
+        val retrofit = RetrofitClient.getInstance();
+        tacTalkAPI = retrofit.create(TacTalkAPI::class.java)
+
+    }
+
+    fun onClick(view: View) {
+        when(view.id){
+            R.id.btn_login -> {
+                // go to login page
+                loginUser(edt_email.text.toString(), edt_password.text.toString())
+            }
+            R.id.goRegister -> {
+                // go to login page
+                startActivity(Intent(this, RegisterFragment::class.java))
+                finish()
+            }
+
+        }
+
+    }
+
+    private fun loginUser(email: String, password: String) {
+        //check empty
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this@LoginFragment, "email can not be null or Empty", Toast.LENGTH_SHORT).show()
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(this@LoginFragment, "Password can not be null or Empty", Toast.LENGTH_SHORT).show()
+            return;
+        }
+
+        compositeDisposable.addAll(tacTalkAPI.loginUser(email, password)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { result ->
+                Toast.makeText(this@LoginFragment, "" +result, Toast.LENGTH_SHORT).show()
+            })
+    }
+}
