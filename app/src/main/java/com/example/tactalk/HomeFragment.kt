91a072e2e9cebdaeeddb2861a34a1947ebc0c378
francisquinn <francisquinnn@gmail.com>
2021-02-05 15:17:49 +0000

    Basic recorder added
    - Added the WAV audio recorder to the mobile app
    - Changed fragments to activities to fix recorder errors
    - Recorder starts when "Start Match" button is pressed
    - Recorder stops when "End Half" button is pressed
    - Audio file stores in the application cache

--- app/src/main/java/com/example/tactalk/HomeFragment.kt
+++ app/src/main/java/com/example/tactalk/HomeFragment.kt
@@ -1,33 +1,32 @@
 package com.example.tactalk
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_home_screen.view.*
+import androidx.appcompat.app.AppCompatActivity
 
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class HomeFragment : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+        setContentView(R.layout.fragment_home_screen)
+
+        val stopButton : Button = findViewById(R.id.GoMainMenu)
+
+        stopButton.setOnClickListener{
+            Toast.makeText(this, "pressed!", Toast.LENGTH_SHORT).show()
         }
+
     }
 
-
-    override fun onCreateView(
+   /* override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
@@ -45,26 +44,8 @@ class HomeFragment : Fragment() {
 
 
         return view
-    }
+    }*/
 
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainMenuFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainMenuFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
