c1eddf0c9390cbdc7871d8d1c0dddfe1c0a76449
kui66de <37660690+kui66de@users.noreply.github.com>
2021-02-28 14:29:00 +0800

    update for sprint 9/10 works

---
+++ app/src/androidTest/java/com/example/tactalk/SetUpMatchTest.kt
@@ -0,0 +1,11 @@
+package com.example.tactalk
+
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class SetUpMatchTest {
+
+}
